8307e0a5e4069f5f7183fa35b474b9b35dc35cd4
TheTosS <jermolajevas@web.de>
2025-03-09 13:03:40 +0100

    hw h1 les2

--- Lesson02/practice/🟣hard/hard01.py
+++ Lesson02/practice/🟣hard/hard01.py
@@ -1,3 +1,8 @@
+
+
 text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor bibendum nisi ut convallis ante "
 a = text.split()
-print(a)
+
+print("слова длинее 5 букв:",a[5:7])
+print(a[9:11])
+print("['",a[-2],"']")
