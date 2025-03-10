aa5a4f74c139a78a51685b2a985c1594a775e92f
TheTosS <jermolajevas@web.de>
2025-03-10 20:05:53 +0100

    les3

---
+++ urok.py
@@ -0,0 +1,9 @@
+from distlib.util import ensure_slash
+
+cost = int(input())
+
+if  cost > 1001 :
+      cost = cost - cost * 0.15
+      print(cost)
+print("sale15%")
+
---
+++ urok2.py
@@ -0,0 +1,10 @@
+hour =int(input("h:"))
+minutes=int(input("m:"))
+
+if hour < 24  and minutes < 60 :
+    print("может")
+
+else:
+    print("не может")
+
+
