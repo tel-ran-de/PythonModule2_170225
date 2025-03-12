349a418c0c59284d756af170a3bd07b18cced744
TheTosS <jermolajevas@web.de>
2025-03-12 19:43:33 +0100

    ispravlenie

--- Lesson03/home_work/02_hw_if.py
+++ Lesson03/home_work/02_hw_if.py
@@ -1,7 +1,7 @@
 a = int(input("Количество коров"))
-if a % 10 == 1 and a != 11 :
+if a % 100 == 1 and a != 11 :
     print("Корова")
-elif a % 10 == 2 or a % 10 == 3 or a % 10 == 4  :
+elif a % 100 == 2 or a % 100 == 3 or a % 100 == 4 and a % 100 != 12 and a % 100 != 13 and a % 100 != 14 :
     print("Коровы")
 else:
     print("Коров")
