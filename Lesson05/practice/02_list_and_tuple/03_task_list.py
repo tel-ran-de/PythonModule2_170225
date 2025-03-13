98167bfe0a48c9ce15c45cf5d2295b542af793e7
TheTosS <jermolajevas@web.de>
2025-03-13 21:00:53 +0100

    practi

--- Lesson05/practice/02_list_and_tuple/03_task_list.py
+++ Lesson05/practice/02_list_and_tuple/03_task_list.py
@@ -1,4 +1,8 @@
 # Дан список из n элементов, заполненный произвольными целыми числами в диапазоне от -10 до 10.
 # Вывести на экран сумму всех элементов.
+numers = [-4,9,-3,8,9]
 
-# TODO: your code here
+print(str(numers))
+print(numers[0]+numers[1]+numers[2]+numers[3]+numers[4])
+
+# TODO: your code here1
