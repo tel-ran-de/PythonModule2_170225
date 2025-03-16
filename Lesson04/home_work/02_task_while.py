106c233fa3e4c5640357a872554d88219b0749c7
TheTosS <jermolajevas@web.de>
2025-03-16 13:16:51 +0100

    homework lesson 4

--- Lesson04/home_work/02_task_while.py
+++ Lesson04/home_work/02_task_while.py
@@ -1,7 +1,16 @@
+
+
 n = int(input("Количество карточек: "))
 # Цикл, который выполнится n-1 раз
-while n >= 1:
-    card_number = int(input("Номер карточки: "))
-    n = n - 1
+sum1 = sum(range(1, n + 1))
+card = 0
 
-print("Номер потерянной карточки:", )
+count = 0
+
+while count < n - 1:
+    card_number = int(input("Номер карточки: "))
+    card += card_number
+    count += 1
+
+lost_card = sum1 - card
+print("Номер потерянной карточки:",lost_card )
