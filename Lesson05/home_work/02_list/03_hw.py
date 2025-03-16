f3004f641349ffcd225e38a30621d398e9533a17
SergeyMichel <michelsergey@gmx.de>
2025-03-16 20:35:02 +0100

    Update 03_hw.py
    lektion5_hw_3

--- Lesson05/home_work/02_list/03_hw.py
+++ Lesson05/home_work/02_list/03_hw.py
@@ -2,3 +2,11 @@
 # Вывести на экран сумму всех положительных элементов кратных двум.
 
 # TODO: your code here
+import random
+
+n = 8
+numbers = [random.randint(-100, 100) for _ in range(n)]
+sum_posit_num = sum(num for num in numbers if num > 0 and num % 2 == 0)
+
+print("список n: ", numbers)
+print("сумму всех n: ", sum_posit_num)
