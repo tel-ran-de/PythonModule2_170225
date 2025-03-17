25ea34a8ca425f5aa9c01cdf77c84c92dd21a82d
TheTosS <jermolajevas@web.de>
2025-03-17 20:41:23 +0100

    mm

--- urok.py
+++ urok.py
@@ -1,9 +1,20 @@
-from distlib.util import ensure_slash
 
-cost = int(input())
 
-if  cost > 1001 :
-      cost = cost - cost * 0.15
-      print(cost)
-print("sale15%")
+text = ("hallo")
 
+#for char in text:
+      #print(char)
+
+
+iterator =iter(text)
+
+el = next(iterator)
+
+print(el)
+
+el = next(iterator)
+
+print(el)
+
+el =  next(iterator)
+print(el)
