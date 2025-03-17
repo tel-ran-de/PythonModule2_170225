34fbc4f5faebd85f472daa2aef20727c132346ac
Mykhailo <vishnevskiy.mihail@gmail.com>
2025-03-17 18:11:10 +0100

    Update 05_hw.py

--- Lesson05/home_work/02_list/05_hw.py
+++ Lesson05/home_work/02_list/05_hw.py
@@ -1,6 +1,15 @@
 # Дан список имен.
 # Найдите самое длинное имя, если таких имен несколько - выведи любое их них
 
-names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
+names = ["Иван", "Ирина", "Вячеслав", "Выхухоль", "Василий", "Петр"]
+len_name = 0
+i_names = 0
+for i, name in enumerate(names):
+    # print(f"{i} {name} {len(name)}")
+    if len_name < len(name): # <= последнее max
+        len_name = len(name)
+        i_names = i
+print(names[i_names])
 
-# TODO: your code here
+# ChatGPT Das is BimBa!!!   ;-)
+print(f"\n{max(names, key=len)}")
