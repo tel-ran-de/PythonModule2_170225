e09350a302ede2064bec94b77ae718cd2b17c47b
Mykhailo <vishnevskiy.mihail@gmail.com>
2025-03-17 18:04:28 +0100

    Update 01_hw.py

--- Lesson05/home_work/02_list/01_hw.py
+++ Lesson05/home_work/02_list/01_hw.py
@@ -3,7 +3,16 @@
 
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
 
-# TODO: your code here
+name_count = len(names)
+n = 1
+for name in names:
+    if n >= name_count:
+        print(f"{name}")
+        break
+    print(f"{name}, ",end="")
+    n += 1
+print("*********Вариант 2******")
+print(", ".join(names))
 
 # Пример вывода:
 # Иван, Ирина, Вячеслав, Василий, Петр
