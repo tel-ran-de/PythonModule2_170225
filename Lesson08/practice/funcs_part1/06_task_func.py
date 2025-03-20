7ab07ee1cf1499655fbf4d85a5852f9b8d24f06b
SergeyMichel <michelsergey@gmx.de>
2025-03-20 21:24:58 +0100

    Update 06_task_func.py
    lektioin8_6

--- Lesson08/practice/funcs_part1/06_task_func.py
+++ Lesson08/practice/funcs_part1/06_task_func.py
@@ -1,8 +1,10 @@
 # Напишите функцию, которая проверит, что точка (x, y)
 # находится строго внутри окружности с центром в точке (xc, yc) и радиусом r:
-
+import math
 def point_in_circle(x, y, xc, yc, r):
     # TODO: your code here
-    pass
+    centr = math.sqrt((x - xc) ** 2 + (y - yc) ** 2)
+
+    return centr < r
 
 # Не забудьте протестировать вашу функцию
