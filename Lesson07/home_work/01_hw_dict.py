6e91fda56c697c678354e56b62531e88ea97f605
TheTosS <jermolajevas@web.de>
2025-03-23 12:49:24 +0100

    hw_lesson7

--- Lesson07/home_work/01_hw_dict.py
+++ Lesson07/home_work/01_hw_dict.py
@@ -3,4 +3,10 @@
 # которая удаляет элемент из словаря по ключу. Если ключ отсутствует, ничего не делает.
 
 my_dict = {"a": 1, "b": 2, "c": 3}
-key = input("Enter key: ")
+key = input("Enter key to delete: ")
+
+if key in my_dict:
+    del my_dict[key]
+    print(my_dict)
+else:
+    print("Key not found")
