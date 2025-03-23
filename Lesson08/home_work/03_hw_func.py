b0af0656b56be0b218131c4c2acfe9f9f32dc7f3
Mykhailo <vishnevskiy.mihail@gmail.com>
2025-03-23 20:00:16 +0100

    Update 03_hw_func.py

--- Lesson08/home_work/03_hw_func.py
+++ Lesson08/home_work/03_hw_func.py
@@ -1,4 +1,24 @@
 # Даны координаты центров двух окружностей (x1; y1) (x2; y2) и и их радиусы  R1 и R2.
 # Находится ли одна окружность целиком внутри другой
 
-# TODO: your code here
+import math
+
+def is_one_circle_inside_another(x1, y1, r1, x2, y2, r2):
+    d = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    if r1 > r2:
+        return d <= r1 - r2
+    else:
+        return d <= r2 - r1
+
+x1 = 0
+y1 = 0
+r1 = 2
+x2 = 3
+y2 = 4
+r2 = 5
+
+result = is_one_circle_inside_another(x1, y1, r1, x2, y2, r2)
+if result:
+    print("Одна окружность находится внутри другой")
+else:
+    print("Окружности не находятся друг в друге")
