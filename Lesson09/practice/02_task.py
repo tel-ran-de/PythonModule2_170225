46d1fef05bedf738287d2dc1a3ce8128337899b4
arkadiikhadzhyohlo <arkada.hadjioglo@gmail.com>
2025-03-24 21:24:06 +0100

    Update 02_task.py

--- Lesson09/practice/02_task.py
+++ Lesson09/practice/02_task.py
@@ -1,1 +1,9 @@
 # Напишите функцию, которая принимает список чисел и возвращает сумму квадратов этих чисел.
+def sum_squares(digits):
+    suma = 0
+    for digit in digits:
+        suma += digit ** 2
+        return suma
+
+digits = [2, 3]
+print(sum_squares(digits))
