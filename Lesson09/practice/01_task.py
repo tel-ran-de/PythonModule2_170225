92b73f0abd96ffa135a748fb7fdcf6d2ead3d175
boo <booblegum42@gmail.com>
2025-03-26 20:59:18 +0545

    lesson09 result

--- Lesson09/practice/01_task.py
+++ Lesson09/practice/01_task.py
@@ -1,2 +1,20 @@
 # Напишите функцию, которая принимает список строк и возвращает новый список,
 # где каждая строка преобразована в верхний регистр.
+
+strings = ["hello", "world", "my", "age"]
+
+def to_upper_case(string):
+    return string.upper()
+
+# Вариант-1:
+# upper_strings = []
+# for string in strings:
+#     upper_strings.append(string.upper())
+
+# Вариант-2:
+# upper_strings = [string.upper() for string in strings]
+
+# Вариант-3:
+upper_strings = list(map(to_upper_case, strings))
+
+print(upper_strings)
--- Lesson09/practice/02_task.py
+++ Lesson09/practice/02_task.py
@@ -1,1 +1,9 @@
 # Напишите функцию, которая принимает список чисел и возвращает сумму квадратов этих чисел.
+
+def sqrt(number):
+    return number * number
+
+numbers = [4, 6, 2, -5, 10, 3]
+
+res = sum(map(sqrt, numbers))
+print(res)
