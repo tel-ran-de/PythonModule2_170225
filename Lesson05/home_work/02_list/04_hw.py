41b702ade7970aa6573ea0dc0ec97aaa43fc403e
Oleksandra247 <alexandra.yakymenko@gmail.com>
2025-03-26 19:01:11 +0100

    Update 04_hw.py

--- Lesson05/home_work/02_list/04_hw.py
+++ Lesson05/home_work/02_list/04_hw.py
@@ -5,3 +5,15 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+import math
+
+input_list = [2, -5, 8, 9, -25, 25, 4]
+output_list = []
+
+for num in input_list:
+    if num >= 0:
+        root = math.sqrt(num)
+        if root.is_integer():
+            output_list.append(int(root))
+
+print(output_list)
