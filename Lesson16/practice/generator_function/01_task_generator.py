2d0054f55b49b83d34ba23a6710b0c5b7b71c31a
arkadiikhadzhyohlo <arkada.hadjioglo@gmail.com>
2025-04-09 20:23:59 +0200

    Update 01_task_generator.py

--- Lesson16/practice/generator_function/01_task_generator.py
+++ Lesson16/practice/generator_function/01_task_generator.py
@@ -2,4 +2,7 @@
 # Напишите генераторную функцию even_numbers(n), которая генерирует все четные числа от 0 до n включительно.
 
 def even_numbers(n):
-    pass
+    for number in range (0,n+1,2):
+        yield number
+
+print(list(even_numbers(10)))
