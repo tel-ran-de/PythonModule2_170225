8e109c4a59a451dd1ebcca4afcdfc2c274e36b5a
boo <booblegum42@gmail.com>
2025-04-18 01:38:24 +0545

    Lesson20 result

--- Lesson20/pactice/window_sliding/01_task_window.py
+++ Lesson20/pactice/window_sliding/01_task_window.py
@@ -1,2 +1,22 @@
 # Дан список целых чисел и число k.
 # Найти максимальную сумму последовательных элементов списка длины k.
+
+def get_max_sum(numbers, k):
+    left = 0
+    right = k - 1
+    max_sum = 0
+    while right < len(numbers):
+        window = numbers[left: right + 1]
+        sum_elements = sum(window)
+        if sum_elements > max_sum:
+            max_sum = sum_elements
+        left += 1
+        right += 1
+    return max_sum
+
+
+assert get_max_sum([2, 12, 4, 26, -4, 0, 22, 1, 5, 8], 3) == 42
+assert get_max_sum([2, 12, 4, 6, -4, 0, 22, 1, 5, 8], 3) == 28
+assert get_max_sum([2, 12, 4, 6, -4, 0, 2, 1, 5], 3) == 22
+assert get_max_sum([2, 12], 2) == 14
+assert get_max_sum([2, 12], 1) == 12
--- Lesson20/pactice/window_sliding/02_task_window.py
+++ Lesson20/pactice/window_sliding/02_task_window.py
@@ -1,2 +1,24 @@
 # Дана строка.
 # Найти длину самой длинной подстроки без повторяющихся символов.
+
+def all_unique(s: str) -> bool:
+    return len(s) == len(set(s))
+
+
+#         l  r
+string = "hello world"
+left = 0
+right = 1
+longest_sub_string = 0
+
+while right < len(string):
+    sub_string = string[left:right + 1]
+    if all_unique(sub_string):
+        if len(sub_string) > longest_sub_string:
+            longest_sub_string = len(sub_string)
+        right += 1
+    else:
+        left = right
+        right = left + 1
+
+print(longest_sub_string)
