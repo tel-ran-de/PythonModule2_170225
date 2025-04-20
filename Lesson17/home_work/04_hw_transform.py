d61b219553537c56715326686537db2128cb3336
TheTosS <jermolajevas@web.de>
2025-04-20 19:48:47 +0200

    Update 04_hw_transform.py

--- Lesson17/home_work/04_hw_transform.py
+++ Lesson17/home_work/04_hw_transform.py
@@ -1,3 +1,5 @@
+
+
 # У вас есть список строк, некоторые из которых содержат email-адреса.
 # Вам нужно извлечь все email-адреса и привести их к нижнему регистру.
 # email-адреса содержат символ '@'.
@@ -9,3 +11,9 @@ log_entries = [
     "Another log",
     "Contact us at support@test.co.uk"
 ]
+
+email_show = list(filter(lambda log_entry: '@' in log_entry, log_entries))
+astring = email_show
+
+email_lower = list(map(lambda email: email.lower(), email_show))
+print(email_lower)
