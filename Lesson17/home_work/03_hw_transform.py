4477e3bfd6a3c1bd6ede86f53a5ca5ba26e24e07
TheTosS <jermolajevas@web.de>
2025-04-20 19:50:30 +0200

    Update 03_hw_transform.py

--- Lesson17/home_work/03_hw_transform.py
+++ Lesson17/home_work/03_hw_transform.py
@@ -9,3 +9,14 @@ students = [
     {'name': 'David', 'grade': 4.2},
     {'name': 'Eve', 'grade': 4.91}
 ]
+# Фильтр оценок > 4,5
+students_note = list(filter(lambda student: student['grade'] > 4.5, students))
+print(f"Студенты, чей средний балл выше 4.5 : {students_note}")
+
+# Средний бал оставшихся оценок
+
+students_not = list(filter(lambda student: student['grade'] < 4.5, students))
+
+summa = sum(students_not['grade'] for students_not in students_not)
+summa2 = summa / 2
+print(f"Средний балл оставшихся студентов: {summa2:.2f}")
