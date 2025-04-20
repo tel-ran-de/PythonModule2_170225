d676727f1d75f008ea14323d1b0f7d6b7b294e55
TheTosS <jermolajevas@web.de>
2025-04-20 19:03:17 +0200

    01_TASK1_sym

--- Lesson20/pactice/part01/01_task_symbols.py
+++ Lesson20/pactice/part01/01_task_symbols.py
@@ -1,7 +1,17 @@
-n = int(input("n: "))  # n = 3
+## "Квадрат символами"
 
-for i in range(n):
-    if i == 0 or i == n - 1:
-        print("*" * n)
-    else:
-        print("*" + " " * (n - 2) + "*")
+
+n = int(input("Введите число от 2 до 19 :"))
+
+if 2 <= n <= 20:
+    for i in range(n):
+        for j in range(n):
+            if i == 0 or i == n - 1 or j == 0 or j == n - 1:
+                print("*", end="")
+
+            else:
+                print(" ", end="")
+        print()
+
+else:
+    print("Введите число в диапазоне от 2 до 19 ! ")
